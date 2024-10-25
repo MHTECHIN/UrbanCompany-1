988eeb49091dfa3ce0c9de6acfa9ff13c27de66d
AtchutaJampana <Atchuta.Jampana@mhtechin.in>
2024-10-25 15:52:48 +0530

    Issues Resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import React from 'react'
-import Home from './pages/Home'
-import Navbar from './pages/Navbar'
-import Slider from './pages/Slider'
-import Noteworthy from './pages/Noteworthy'
-import BookedService from './pages/BookedServices'
+import React from "react";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-
-
       <Navbar />
       <BrowserRouter>
         <Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HomeServices from "../components/HomeServices";
-import Slider from "./Slider";
-import Noteworthy from "./Noteworthy";
+import Slider from "../components/Slider";
+import Noteworthy from "../components/Noteworthy";
 
 export default function HomePage() {
   return (
